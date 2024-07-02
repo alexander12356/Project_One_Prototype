d0de7fa4d6b9fbd1f8d627144089167a60172299
alexander.mekumyanov <alexander12356@inbox.ru>
2024-07-02 11:40:51 +0900

    add welcome window

---
+++ Assets/Scripts/UI/WelcomeWindow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Mech.UI
+{
+	public class WelcomeWindow : MonoBehaviour
+	{
+		public static WelcomeWindow Instance;
+
+		[SerializeField] private CanvasGroup _canvasGroup;
+
+		private void Awake()
+		{
+			Instance = this;
+		}
+
+		private void Start()
+		{
+			SetVisible(true);
+		}
+
+		private void SetVisible(bool value)
+		{
+			_canvasGroup.alpha = value ? 1f : 0f;
+			_canvasGroup.blocksRaycasts = value;
+		}
+
+		public void Close()
+		{
+			SetVisible(false);
+		}
+	}
+}
